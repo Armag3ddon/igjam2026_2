412e9b78eaab890a3d2cb63777063cc9a4997b71
Fravincii <fravinci1802@gmail.com>
2026-09-26 01:00:06 +0200

    Card Shuffle with Animation

---
+++ scripts/cardShuffle.gd
@@ -0,0 +1,49 @@
+extends Node2D
+
+var cards: Array[Node];
+@export var timesToMove: int = 2;
+@export var cardSwitchSpeed: float = 1.5;
+@export var cardJumpHeight: float = 650;
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	cards = $Cardhandler.get_children()
+	print(cards.size())
+	_cardsInit()
+	_MoveCards()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	
+func _cardsInit()-> void:
+	cards.shuffle();
+	
+	
+func _MoveCards()-> void:
+	print("cards.size() = ", cards.size())
+	for i in range(0,timesToMove):
+		var firstCardIndex = randi_range(0,cards.size()-1);
+		var secondCardIndex = randi_range(0,cards.size()-1);
+		while firstCardIndex == secondCardIndex:
+			secondCardIndex = randi_range(0,cards.size()-1);
+		var firstSelectedCard = cards[firstCardIndex];
+		var secondSelectedCard = cards[secondCardIndex];
+		var firstCardOriginalTransform = firstSelectedCard.position
+		var secondCardOriginalTransform = secondSelectedCard.position
+		print ("Nr 1")
+		switchCards(firstSelectedCard,firstCardOriginalTransform,secondCardOriginalTransform,cardJumpHeight,cardSwitchSpeed)
+		print ("Nr 2")
+		await switchCards(secondSelectedCard,secondCardOriginalTransform,firstCardOriginalTransform,-cardJumpHeight,cardSwitchSpeed)
+		#await switchCards(secondSelectedCard,firstCardOriginalTransform,secondCardOriginalTransform,-650,cardSwitchSpeed)
+		
+	
+func switchCards(cardToMove: Node2D,start: Vector2, target: Vector2, height: float, duration: float)-> void:
+	var tween = create_tween()
+	tween.tween_method(
+		func(t: float):
+			var pos = start.lerp(target, t)
+			pos.y -= sin(t * PI) * height
+			cardToMove.position = pos, 0.0, 1.0, duration
+	)
+	await tween.finished
