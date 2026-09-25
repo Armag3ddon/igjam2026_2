c09f609fa8b45af7649ab4ca8b314c3df2ca612d
Felix Wagner <Armag3ddon@web.de>
2026-09-25 23:43:58 +0200

    Basic bird scenes

--- scripts/game.gd
+++ scripts/game.gd
@@ -21,9 +21,11 @@ enum game_states {
 var game_state: int
 
 var dangers: Array[bool] = [false, false, false, false, false]
+var danger_time: float = 0.0
+var danger_wait: float = 3.0
 
 func _ready() -> void:
-	for i in racer_count:
+	for i: int in racer_count:
 		var racer: Racer = racer_scene.instantiate()
 		racers.append(racer)
 		racers[i].setNewPosition(Vector2(i, 10))
@@ -45,4 +47,15 @@ func _on_central_text_text_finished() -> void:
 		game_state = game_states.READY
 
 func drawDanger() -> void:
-	pass
+	game_state = game_states.DANGER
+	dangers = [false, false, false, false, false]
+	var danger_count: int = roundi(randf() * 3.0) + 1
+	for i: int in danger_count:
+		var rnd: int = roundi(randf() * 4.0)
+		while dangers[rnd]:
+			rnd = roundi(randf() * 4.0)
+		dangers[rnd] = true
+
+func _process(delta: float) -> void:
+	if game_state == game_states.DANGER:
+		pass
